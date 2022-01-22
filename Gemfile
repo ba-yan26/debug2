308b4d6b8b9a4d816822ff4b77be4a176128916b
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-01-22 07:04:24 +0000

    [Upload]いいね機能エラー解消

--- Gemfile
+++ Gemfile
@@ -55,4 +55,3 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'bootstrap', '~> 4.5.0'
-gem 'jquery-rails'
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,7 +6,7 @@ class FavoritesController < ApplicationController
     # favorite.user_id = current_user.id
     favorite = current_user.favorites.new(book_id:book.id)
     favorite.save
-    redirect_to books_path(book)
+    redirect_to book_path(book)
   end
 
   def destroy
@@ -15,7 +15,7 @@ class FavoritesController < ApplicationController
     # favorite.user_id = current_user.id
     favorite = current_user.favorites.find_by(book_id:book.id)
     favorite.destroy
-    redirect_to books_path(book)
+    redirect_to book_path(book)
   end
 
 end
