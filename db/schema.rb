cc0ff677b4ce80840c4e7bf05a36e47d06316a10
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-01-23 02:26:05 +0000

    [Start]フォロー機能

--- db/schema.rb
+++ db/schema.rb
@@ -63,6 +63,13 @@ ActiveRecord::Schema.define(version: 2022_01_22_002143) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
