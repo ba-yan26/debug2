b5bc43b0cd16b43f46a84443bbdc56df854ee05b
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-01-26 06:26:10 +0000

    [Upload]非同期通信いいねOK

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,21 +1,21 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     # favorite = Favorite.new(book_id :book.id)
     # favorite.user_id = current_user.id
-    favorite = current_user.favorites.new(book_id:book.id)
-    favorite.save
-    redirect_back(fallback_location: root_path)
+    favorite = current_user.favorites.new(book_id: @book.id)
+    favorite.save!
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     # favorite = Favorite.find_by(book_id :book.id)
     # favorite.user_id = current_user.id
-    favorite = current_user.favorites.find_by(book_id:book.id)
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 
 end
