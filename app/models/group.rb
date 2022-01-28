69b764a7f0315cbd1e8b6b070de16f62ebe7924c
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-01-28 09:58:09 +0000

    [Upload]グループ機能実装,Editボタン表示

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,14 @@
 class Group < ApplicationRecord
+  has_many :group_users
+  has_many :users, through: :group_users
+  # usersはthrough記述で中間テーブルを通じてgroupsにアクセスできるようにする
+
+  validates :name, presence: true
+  validates :introduction, presence: true
+
+  has_one_attached :image
+
+  def get_image
+    (image.attached?) ? image : 'no_image.jpg'
+  end
 end
