bc51b7b33149038ecf331d45bc06d122d333ca16
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-01-30 12:12:57 +0000

    [Upload]メール送信機能

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -58,6 +58,19 @@ class GroupsController < ApplicationController
     # current_userは@group.usersから削除される
     redirect_to groups_path
   end
+  
+  def new_mail
+    @group = Group.find(params[:group_id])
+  end
+  
+  def send_mail
+    @group = Group.find(params[:group_id])
+    group_users = @group.users
+    @mail_title = params[:mail_title]
+    @mail_content = params[:mail_content]
+    ContactMailer.send_mail(@mail_title,@mail_content,group_users).deliver
+    # contact_mailer.rbのsend_mailアクションに前述の内容を渡している
+  end
 
 
   private
